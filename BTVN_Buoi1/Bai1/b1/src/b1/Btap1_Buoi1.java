088135563cb1612fc563e245195888a23a6ae24f
cloudforrest <tuanmc2885@gmail.com>
2020-09-25 21:37:34 +0700

    Vu Trong Tuan

---
+++ BTVN_Buoi1/Bai1/b1/src/b1/Btap1_Buoi1.java
@@ -0,0 +1,18 @@
+package b1;
+
+public class Btap1_Buoi1 {
+	public static void main(String[] args) {
+		int m = 5, n = 3;
+		for(int i = 1; i <= n; ++i) {
+			for(int j = 1; j <= m; ++j) {
+				if(i==1 || i==n || j ==1 || j==m) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println("");
+		}
+	}
+}
---
+++ BTVN_Buoi1/Bai2/b2/src/b2/Btap2_Buoi1.java
@@ -0,0 +1,5 @@
+package b2;
+
+public class Btap2_Buoi1 {
+
+}
