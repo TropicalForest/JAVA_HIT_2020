31ddab4b98f59084cff7239aba0eee5c5ffebfe7
cloudforrest <tuanmc2885@gmail.com>
2020-09-25 22:14:30 +0700

    Vu Trong Tuan

--- BTVN_Buoi1/Bai2/b2/src/b2/Btap2_Buoi1.java
+++ BTVN_Buoi1/Bai2/b2/src/b2/Btap2_Buoi1.java
@@ -1,5 +1,25 @@
 package b2;
 
 public class Btap2_Buoi1 {
-
+	public static void main(String[] args) {
+		System.out.println("*       *     *     * * * *    * * * *    *   *              ");
+		System.out.println("|       |    / \\    |       *  |       *   \\ /                 ");
+		System.out.println("* ----- *   *---*   * * * *    * * * *      *             ");
+		System.out.println("|       |  /     \\  |          |            |          ");
+		System.out.println("*       * *       * *          *            *       ");	
+		System.out.println();
+		System.out.println();
+		System.out.println("*   * ******  *       *       *                      ");
+		System.out.println("|\\  | |        \\     / \\     /                                       ");
+		System.out.println("* * * ******    *   *   *   *                       ");
+		System.out.println("|  \\| |          \\ /     \\ /                                         ");
+		System.out.println("*   * ******      *       *                          ");
+		System.out.println();
+		System.out.println();
+		System.out.println("*   *   *****      *      * * * *              ");
+		System.out.println(" \\ /    |         / \\     |      *            ");
+		System.out.println("  *     *****    *---*    * * * *                    ");
+		System.out.println("  |     |       /     \\   |    \\       ");
+		System.out.println("  *     *****  *       *  *     *           ");
+	}
 }
